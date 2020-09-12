52dfe995ae6b774f343798be1c5ebcb0df6df385
Akshuma Trivedi <akshuma.trivedi@tatadigital.com>
2020-09-12 18:32:34 +0530

    inital code

---
+++ TriSwitch/TriSwitch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TriSwitch
+//
+//  Created by Akshuma Trivedi on 12/09/20.
+//  Copyright © 2020 Akshuma Trivedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
